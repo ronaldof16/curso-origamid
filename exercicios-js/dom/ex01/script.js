bcecff5a53116884286089588e981df4e4fed4d7
ronaldof16 <ronaldof16@hotmail.com>
2023-10-07 14:17:52 -0300

    Resolução do exercício

---
+++ exercicios-js/dom/ex01/script.js
@@ -0,0 +1,19 @@
+// Retorne o url da página atual utilizando o objeto window
+let url = window.location.href;
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+let primeiro = document.querySelector(".ativo").innerHTML;
+console.log(primeiro)
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+
+console.log(linguagem);
+
+// Retorne a largura da janela
+let largura = window.innerWidth;
+console.log(largura);
+
+ 
