3554de1d49c6075ef1007d2a33484327e4391a67
ronaldof16 <ronaldof16@hotmail.com>
2023-10-12 17:33:31 -0300

    Praticando o método scrollTo

--- Animais-fantasticos/js/ex07.js
+++ Animais-fantasticos/js/ex07.js
@@ -41,4 +41,34 @@ function initAcordion() {
 
 initAcordion()
 
+function initScrollSuave() {
+    let linksInternos = document.querySelectorAll(".js-menu a[href^='#");
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        let href = event.currentTarget.getAttribute("href");
+        let section = document.querySelector(href)
+        
+        let topo = section.offsetTop;
+        window.scrollTo({
+            top: topo,
+            behavior: "smooth"
+        })
+
+        /* forma alternativa
+        section.scrollInToView({
+            behavior: "smooth",
+            block: "start"
+        })  */
+
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener("click", scrollToSection)
+    })
+}
+
+initScrollSuave()
+
+
 
