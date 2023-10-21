b7ac772c3dc0c3e720ace6169b86184c9dda7713
ronaldof16 <ronaldof16@hotmail.com>
2023-10-21 16:56:16 -0300

    Resolução do exercício

--- exercicios-js/metodos-string/script.js
+++ exercicios-js/metodos-string/script.js
@@ -30,11 +30,9 @@ let somaRecebimentos = 0;
     const numeroLimpo = +item.valor.replace("R$ ", "");
     if(item.descricao.slice(0, 4) === "Taxa") {
         somaTaxas += numeroLimpo;
-    }
-
-    if(item.descricao.slice(0, 4) === "Rece") {
-        somaRecebimentos += numeroLimpo;
-    }
+    } else if(item.descricao.slice(0, 4) === "Rece") {
+      somaRecebimentos += numeroLimpo;
+  }
   })
 
   console.log(somaTaxas)
@@ -42,6 +40,10 @@ let somaRecebimentos = 0;
   
   // Retorne uma array com a lista abaixo
   const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+  let transportesArray = transportes.split(';');
+  console.log(transportesArray)
+  let transportesString = transportesArray.join(', ');
+  console.log(transportesString)
   
   // Substitua todos os span's por a's
   const html = `<ul>
@@ -49,12 +51,29 @@ let somaRecebimentos = 0;
                   <li><span>Produtos</span></li>
                   <li><span>Contato</span></li>
                 </ul>`;
+
+  let htmlA = html.split("span").join("a");
+  console.log(htmlA)
   
   // Retorne o último caracter da frase
   const frase = 'Melhor do ano!';
+  let frase2 = frase.charAt(frase.length - 1)
+  console.log(frase2)
   
   // Retorne o total de taxas
-  //const transacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+  const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+let totalTaxas = 0;
+transacoes2.forEach((item) => {
+  item = item.trim().toLowerCase().slice(0,4)
+  if(item === "taxa") {
+    totalTaxas++
+  }
+})
+
+console.log(totalTaxas)
+
+
+
   
   
   
