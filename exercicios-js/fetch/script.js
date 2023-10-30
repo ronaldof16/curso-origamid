ff08ff5aa6fb9c6c37fff6b15f16820742f06dc7
ronaldof16 <ronaldof16@hotmail.com>
2023-10-30 09:42:10 -0300

    resolução do exercício

---
+++ exercicios-js/fetch/script.js
@@ -0,0 +1,57 @@
+// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// crie um formulário onde o usuário pode digitar o cep
+// e o endereço completo é retornado ao clicar em buscar
+const inputCep = document.querySelector("#cep")
+const btnCep = document.querySelector("#btnCep")
+const resultadoCep = document.querySelector(".resultadoCep")
+
+btnCep.addEventListener("click", handleClick)
+
+function handleClick(event) {
+    event.preventDefault();
+    const cep = inputCep.value;
+    buscaCep(cep)
+}
+
+function buscaCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.text())
+    .then(body => {
+        resultadoCep.innerText = body;
+    })
+}
+
+// Utilizando a API https://blockchain.info/ticker
+// retorne no DOM o valor de compra da bitcoin and reais.
+// atualize este valor a cada 30s
+const btcDisplay = document.querySelector(".btc");
+
+function fetchBtc() {
+    fetch("https://blockchain.info/ticker")
+    .then(response => response.json())
+    .then(btcJson => {
+        btcDisplay.innerText = ("R$ " + btcJson.BRL.buy).replace(".", ",")
+    })
+}
+
+//setInterval(fetchBtc, 1000)
+fetchBtc()
+
+// Utilizando a API https://api.chucknorris.io/jokes/random
+// retorne uma piada randomica do chucknorris, toda vez que
+// clicar em próxima
+const textoPiada = document.querySelector(".piada");
+const btn = document.querySelector(".btn")
+
+function passarPiada() {
+    fetch("https://api.chucknorris.io/jokes/random")
+    .then(response => response.json())
+    .then(piada => {
+        textoPiada.innerText = piada.value
+    })
+}
+
+passarPiada();
+
+btn.addEventListener("click", passarPiada)
+
