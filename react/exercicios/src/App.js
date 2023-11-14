d22068a40e495db8386dff92667ab35f39ec0459
ronaldof16 <ronaldof16@hotmail.com>
2023-11-14 15:36:24 -0300

    Resolução do exercício de JSX

---
+++ react/exercicios/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ExJsx from './componentes/ExJsx';
+
+function App() {
+  return (
+    <div className="App">
+      <ExJsx />
+    </div>
+  );
+}
+
+export default App;
