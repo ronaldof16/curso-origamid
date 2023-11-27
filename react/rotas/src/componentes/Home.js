28d3458e1da67da095920cafe45d8f38c53f0b2a
ronaldof16 <ronaldof16@hotmail.com>
2023-11-27 10:51:24 -0300

    Alterando o título e a descrição na tag meta do head

--- react/rotas/src/componentes/Home.js
+++ react/rotas/src/componentes/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Head from './Head'
 
 const Home = () => {
   return (
     <div>
-        <h1>Home</h1>
-        <p>Essa é a página Home</p>
-        <Link to='produto/notebook'>Notebook</Link> {' '}
-        <Link to='produto/smartphone'>Smartphone</Link>
+      <Head title='Home' description='Essa é a descrição da Home' />
+      <h1>Home</h1>
+      <p>Essa é a página Home</p>
+      <Link to='produto/notebook'>Notebook</Link> {' '}
+      <Link to='produto/smartphone'>Smartphone</Link>
     </div>
   )
 }
--- react/rotas/src/componentes/Login.js
+++ react/rotas/src/componentes/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import Head from './Head';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -10,6 +11,7 @@ const Login = () => {
 
     return (
         <div>
+            <Head title='Login' description='Essa é a descrição do Login' />
             <h1>Login</h1>
             <button onClick={handleClick}>Login</button>
         </div>
